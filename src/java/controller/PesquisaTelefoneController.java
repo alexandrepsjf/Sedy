166d1957f0a5ab0d02c93f5ff544a2501f83c0c6
rfbezerra <rfbezerra@STFJFAC022903-W.stefanini.dom>
2017-06-01 15:44:33 -0300

    Refatoração PesquisaTelefoneController

--- src/java/controller/PesquisaTelefoneController.java
+++ src/java/controller/PesquisaTelefoneController.java
@@ -5,6 +5,8 @@
  */
 package controller;
 
+
+import dao.TelefoneDAO;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.SQLException;
@@ -31,18 +33,14 @@ public class PesquisaTelefoneController extends HttpServlet {
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
+     * @throws java.lang.ClassNotFoundException
      * @throws java.sql.SQLException
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException, SQLException {
-        response.setContentType("text/html;charset=UTF-8");
-        try {
-            request.setAttribute("telefones", Telefone.obterTelefones());
-                        RequestDispatcher view
-                    = request.getRequestDispatcher("/pesquisaTelefone.jsp");
-            view.forward(request, response);
-        } catch (ClassNotFoundException ex) {
-        }
+            throws ServletException, IOException {
+        request.setAttribute("telefone", TelefoneDAO.getInstance().getAllTelefones());
+        RequestDispatcher view = request.getRequestDispatcher("/pesquisarTelefone.jsp");
+        view.forward(request, response);
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
@@ -57,11 +55,21 @@ public class PesquisaTelefoneController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        try {
-            processRequest(request, response);
-        } catch (SQLException ex) {
-            Logger.getLogger(PesquisaClienteController.class.getName()).log(Level.SEVERE, null, ex);
-        }
+        processRequest(request, response);
+    }
+ // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    /*@Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
     }
 
     /**
@@ -75,11 +83,7 @@ public class PesquisaTelefoneController extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        try {
-            processRequest(request, response);
-        } catch (SQLException ex) {
-            Logger.getLogger(PesquisaClienteController.class.getName()).log(Level.SEVERE, null, ex);
-        }
+        processRequest(request, response);
     }
 
     /**
@@ -91,5 +95,4 @@ public class PesquisaTelefoneController extends HttpServlet {
     public String getServletInfo() {
         return "Short description";
     }// </editor-fold>
-
 }
