e0d5bf55d6e471b08356fafbcdc4dac109724a96
fanuel nunes <fanuelson@gmail.com>
2017-06-29 00:38:39 -0300

    Implementação tela home.

--- src/main/java/controller/HomeController.java
+++ src/main/java/controller/HomeController.java
@@ -1,14 +1,14 @@
 package controller;
 
 import java.io.Serializable;
-import java.util.Date;
 
 import javax.annotation.PostConstruct;
 import javax.faces.view.ViewScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
 
-import org.apache.log4j.Logger;
+import dao.BairroDAO;
+import model.Bairro;
 
 @Named
 @ViewScoped
@@ -16,11 +16,25 @@ public class HomeController implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
-    static final Logger log = Logger.getLogger(HomeController.class);
-
+    @Inject
+    private Bairro bairro;
+    
     @PostConstruct
     public void init() {
         System.out.println("oi");
     }
+    
+    public void testar() {
+    	BairroDAO.getInstance().getAllBairros();
+    }
+
+	public Bairro getBairro() {
+		return bairro;
+	}
+
+	public void setBairro(Bairro bairro) {
+		this.bairro = bairro;
+	}
+    
 
 }
